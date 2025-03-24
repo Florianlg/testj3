661b9f0188666155a48212647f8506e08e3db93b
Florian <contact@empreintesdigitales.fr>
2025-03-24 15:06:12 +0100

    first

--- projet/frontend/vite.config.ts
+++ projet/frontend/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, loadEnv, ConfigEnv, UserConfigExport } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default ({ mode }: ConfigEnv): UserConfigExport =>  {
+export default ({mode}) => {
   // On charge les variables d'environnement à partir du docker-compose et à défaut du fichier .env
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
